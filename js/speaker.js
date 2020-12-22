60452b5b26147b42302526a967744ce68dede026
mneunomne <alberto.harres@gmail.com>
2020-12-22 21:17:27 +0100

    first commit

---
+++ js/speaker.js
@@ -0,0 +1,53 @@
+/**
+ * Creates a grid of entities based on the given template
+ * Place at the center of the grid you wish to create
+ * Requires aframe-template-component
+ */
+AFRAME.registerComponent('speaker', {
+  schema: {
+    name: {type: 'string'}
+  },
+
+  init: function() {
+    var el = this.el;
+    var center = el.getAttribute('position');
+
+    const randomColor = Math.floor(Math.random()*16777215).toString(16)
+    
+    el.setAttribute('color', "#" + randomColor)
+
+    console.log(window.speakers[this.data.name], this.data.name)
+
+    const audios = window.speakers[this.data.name].map(n => n.replace('.wav',''))
+
+    var index = 0
+
+    document.querySelector('button').addEventListener('click', function() {
+      el.setAttribute('sound', 'src: #' + audios[index] + ';')
+    })
+
+    el.addEventListener('sound-ended', (evt) => {
+      index = index === audios.length-1 ? 0 : index+1 
+      el.setAttribute('sound', 'src: #' + audios[index] + ';')
+    })
+
+    el.addEventListener('sound-loaded', (evt) => {
+      el.components.sound.playSound();
+    })
+
+    const area = 20 
+    var worldPoint = {x: center.x + (Math.random() * area - area/2), y: center.y, z: center.z + (Math.random() * area - area/2)};
+    el.setAttribute('position', worldPoint);
+
+  },
+
+  getRandomAngleInRadians: function() {
+    return Math.random()*Math.PI*2;
+  },
+
+  randomPointOnCircle: function (radius, angleRad) {
+    var x = Math.cos(angleRad)*radius;
+    var y = Math.sin(angleRad)*radius;
+    return {x: x, y: y};
+  }
+});
