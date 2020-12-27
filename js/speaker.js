df87c534be12a93a1db5c6d2b5d1ba52c2208905
mneunomne <alberto.harres@gmail.com>
2020-12-27 21:54:35 +0100

    change viz system, new audios, db

--- js/speaker.js
+++ js/speaker.js
@@ -5,57 +5,67 @@
  */
 AFRAME.registerComponent('speaker', {
   schema: {
-    name: {type: 'string'}
+    name: {type: 'string'},
+    name_id: {type: 'string'}
   },
 
   init: function() {
     var el = this.el;
     var center = el.getAttribute('position');
-
     const randomColor = Math.floor(Math.random()*16777215).toString(16)
-    
     el.setAttribute('color', "#" + randomColor)
-
-    console.log(window.speakers[this.data.name], this.data.name)
-
-    var name = this.data.name
-
     var index = 0
 
-    var audios = window.speakers[this.data.name].map(n => n.replace('.wav',''))
+    let audio_list = window.db_audios.filter(a => a.from === this.data.name)
+
+
+    var name_target_el_wrapper = document.createElement('div');
+    name_target_el_wrapper.setAttribute('id', 'wrapper-' + this.data.name_id)
+    name_target_el_wrapper.setAttribute('style', "width: 100%; height: 100%; position: fixed; left: 0; top: 0; z-index: -1; overflow: hidden")
+    document.body.appendChild(name_target_el_wrapper)
+
+
+    var name_target_el = document.createElement('div');
+    name_target_el.setAttribute('style', "background: #000000; color: white; font-size: 46px; display: inline-block;")
+    name_target_el.setAttribute('id', this.data.name_id)
+    name_target_el.innerText = this.data.name
+    document.querySelector('#wrapper-' + this.data.name_id).appendChild(name_target_el)
+
+    this.name_target_el = name_target_el
+
+    var name_el = document.createElement('a-entity');
+    name_el.setAttribute('geometry', `primitive: plane;`)
+    name_el.setAttribute('look-at', `#camera`)
+    name_el.setAttribute('position', `0 0.6 -0.5;`)
+    name_el.setAttribute('material', `shader: html; target: #${this.data.name_id}; ratio: height;`)
+    el.appendChild(name_el)
     
-    
-    audios.map((n, i) => {
+    audio_list.map((a, i) => {
       // create audio entity
       var _el = document.createElement('a-entity');
-      _el.setAttribute('sound', 'src: #' + audios[i] + ';')
-      _el.setAttribute('id', this.data.name + i)
+      let audio_id = a.file.replace('.mp3', '').replace('convert_dest/', "")
+      _el.setAttribute('sound', 'src: #' + audio_id + '; distanceModel: exponential;')
+      _el.setAttribute('id', this.data.name_id + i)
       _el.addEventListener('sound-ended', (evt) => {
-        console.log('sound ended', evt)
-        index = index === audios.length-1 ? 0 : index+1
+        index = index === audio_list.length-1 ? 0 : index+1
         this.playSound(index)
       })
       el.appendChild(_el)
     })
 
-
     document.querySelector('button').addEventListener('click', () => {
-      // el.setAttribute('sound', 'src: #' + audios[index] + ';  autoplay: true; poolSize: 20;')
-      console.log('index', index)
       this.playSound(index)
     })
 
     const area = 20 
-    var worldPoint = {x: center.x + (Math.random() * area - area/2), y: center.y, z: center.z + (Math.random() * area - area/2)};
+    var worldPoint = {x: center.x + (Math.random() * area - area/2), y: center.y + (Math.random() * 10 - 10/2), z: center.z + (Math.random() * area - area/2)};
     el.setAttribute('position', worldPoint);
 
   },
 
   playSound: function(index) {
-    let el = this.el.querySelector('#' + this.data.name + index)
-
-    console.log('el', el, this.data.name, index)
-
-    this.el.querySelector('#' + this.data.name + index).components.sound.playSound()
+    console.log('this.data.name_id', this.data.name_id)
+    this.el.querySelector('#' + this.data.name_id + index).components.sound.playSound()
+    this.name_target_el.innerText = index
   }
 });
--- js/speakers.js
+++ js/speakers.js
@@ -124,3 +124,635 @@ window.speakers = {
     'echoes-89.wav',
   ]
 }
+
+window.db_audios = [
+  {
+      "from": "Alberto Harres",
+      "file": "convert_dest/audio_1@15-12-2020_14-57-52.mp3",
+      "id": 13,
+      "next_text": "Números"
+  },
+  {
+      "from": "nilya musaeva",
+      "file": "convert_dest/audio_2@15-12-2020_14-58-37.mp3",
+      "id": 15,
+      "next_text": "еда"
+  },
+  {
+      "from": "S. C.",
+      "file": "convert_dest/audio_3@15-12-2020_16-21-06.mp3",
+      "id": 22,
+      "next_text": ""
+  },
+  {
+      "from": "Alberto Harres",
+      "file": "convert_dest/audio_4@15-12-2020_16-31-05.mp3",
+      "id": 27,
+      "next_text": "怪迷日眼"
+  },
+
+  {
+      "from": "Alberto Harres",
+      "file": "convert_dest/audio_5@15-12-2020_17-10-04.mp3",
+      "id": 30,
+      "next_text": "Ensimesmado"
+  },
+
+  {
+      "from": "曦 若",
+      "file": "convert_dest/audio_6@15-12-2020_17-11-15.mp3",
+      "id": 32,
+      "next_text": "造孽巴撒"
+  },
+
+  {
+      "from": "曦 若",
+      "file": "convert_dest/audio_7@15-12-2020_17-20-54.mp3",
+      "id": 34,
+      "next_text": "遇球得到"
+  },
+
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_8@15-12-2020_17-36-43.mp3",
+      "id": 36,
+      "next_text": "ロックダウン"
+  },
+
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_9@15-12-2020_17-37-06.mp3",
+      "id": 39,
+      "next_text": "非常事態宣言"
+  },
+
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_10@15-12-2020_17-37-48.mp3",
+      "id": 41,
+      "next_text": "20秒間"
+  },
+
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_11@15-12-2020_17-38-22.mp3",
+      "id": 43,
+      "next_text": "三密"
+  },
+
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_12@15-12-2020_17-41-09.mp3",
+      "id": 45,
+      "next_text": "Zoom会議"
+  },
+
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_13@15-12-2020_17-41-53.mp3",
+      "id": 47,
+      "next_text": "コロナに効く！"
+  },
+
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_14@15-12-2020_17-42-02.mp3",
+      "id": 49,
+      "next_text": "コロナに負けない"
+  },
+
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_15@15-12-2020_17-42-35.mp3",
+      "id": 51,
+      "next_text": "コロナ下"
+  },
+
+
+
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_16@15-12-2020_17-43-50.mp3",
+      "id": 56,
+      "next_text": "閉店しました"
+  },
+
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_17@15-12-2020_17-44-55.mp3",
+      "id": 58,
+      "next_text": "こんなご時世ですが"
+  },
+
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_18@15-12-2020_17-45-04.mp3",
+      "id": 60,
+      "next_text": "また会う日まで"
+  },
+
+
+
+  {
+      "from": "Hem Poudyal",
+      "file": "convert_dest/audio_19@15-12-2020_17-55-57.mp3",
+      "id": 67,
+      "next_text": "कोरोनाको समय, एक्लोपन अनि असहजता"
+  },
+
+  {
+      "from": "Antônio Hof",
+      "file": "convert_dest/audio_20@15-12-2020_18-02-26.mp3",
+      "id": 70,
+      "next_text": "Abacateiro"
+  },
+
+  {
+      "from": "Antônio Hof",
+      "file": "convert_dest/audio_21@15-12-2020_18-02-45.mp3",
+      "id": 72,
+      "next_text": "pimpolho"
+  },
+
+  {
+      "from": "Antônio Hof",
+      "file": "convert_dest/audio_22@15-12-2020_18-02-53.mp3",
+      "id": 74,
+      "next_text": "Rabiola"
+  },
+
+  {
+      "from": "Hem Poudyal",
+      "file": "convert_dest/audio_23@15-12-2020_18-08-21.mp3",
+      "id": 76,
+      "next_text": "संवेदनशील मानव जीवन"
+  },
+
+  {
+      "from": "Hem Poudyal",
+      "file": "convert_dest/audio_24@15-12-2020_18-10-32.mp3",
+      "id": 78,
+      "next_text": "बाध्यता"
+  },
+
+  {
+      "from": "Alberto Harres",
+      "file": "convert_dest/audio_25@15-12-2020_18-12-33.mp3",
+      "id": 80,
+      "next_text": "Sirenes"
+  },
+
+  {
+      "from": "Alberto Harres",
+      "file": "convert_dest/audio_26@15-12-2020_18-13-47.mp3",
+      "id": 82,
+      "next_text": "Respiradores"
+  },
+
+  {
+      "from": "Alberto Harres",
+      "file": "convert_dest/audio_27@15-12-2020_18-19-53.mp3",
+      "id": 84,
+      "next_text": "Esperança"
+  },
+
+  {
+      "from": "Antônio Hof",
+      "file": "convert_dest/audio_28@15-12-2020_20-16-18.mp3",
+      "id": 87,
+      "next_text": "Alemão"
+  },
+
+  {
+      "from": "nilya musaeva",
+      "file": "convert_dest/audio_29@15-12-2020_20-16-35.mp3",
+      "id": 89,
+      "next_text": "система цифровых пропусков"
+  },
+
+  {
+      "from": "S. C.",
+      "file": "convert_dest/audio_30@16-12-2020_13-17-48.mp3",
+      "id": 91,
+      "next_text": "宅"
+  },
+
+  {
+      "from": "S. C.",
+      "file": "convert_dest/audio_31@16-12-2020_13-20-38.mp3",
+      "id": 93,
+      "next_text": "健康码"
+  },
+
+  {
+      "from": "Chi Him Chik",
+      "file": "convert_dest/audio_32@16-12-2020_14-13-07.mp3",
+      "id": 98,
+      "next_text": "第一波"
+  },
+
+  {
+      "from": "Chi Him Chik",
+      "file": "convert_dest/audio_33@16-12-2020_14-13-19.mp3",
+      "id": 100,
+      "next_text": "第二波"
+  },
+
+  {
+      "from": "Chi Him Chik",
+      "file": "convert_dest/audio_34@16-12-2020_14-13-27.mp3",
+      "id": 102,
+      "next_text": "第三波"
+  },
+
+  {
+      "from": "Chi Him Chik",
+      "file": "convert_dest/audio_35@16-12-2020_14-13-36.mp3",
+      "id": 104,
+      "next_text": "第四波"
+  },
+
+  {
+      "from": "Sangbong Lee",
+      "file": "convert_dest/audio_36@16-12-2020_14-15-39.mp3",
+      "id": 106,
+      "next_text": "코로나 감염병 확산"
+  },
+
+  {
+      "from": "Sangbong Lee",
+      "file": "convert_dest/audio_37@16-12-2020_14-15-51.mp3",
+      "id": 108,
+      "next_text": "확진자"
+  },
+
+  {
+      "from": "Sangbong Lee",
+      "file": "convert_dest/audio_38@16-12-2020_14-16-06.mp3",
+      "id": 110,
+      "next_text": "가짜뉴스와 음모론"
+  },
+
+  {
+      "from": "Sangbong Lee",
+      "file": "convert_dest/audio_39@16-12-2020_14-16-16.mp3",
+      "id": 112,
+      "next_text": "격리시설"
+  },
+
+  {
+      "from": "Sangbong Lee",
+      "file": "convert_dest/audio_40@16-12-2020_14-49-03.mp3",
+      "id": 114,
+      "next_text": "가짜뉴스와 음모론"
+  },
+
+  {
+      "from": "Alberto Harres",
+      "file": "convert_dest/audio_41@16-12-2020_15-16-56.mp3",
+      "id": 116,
+      "next_text": "Protocolos de distanciamento social"
+  },
+
+  {
+      "from": "Alberto Harres",
+      "file": "convert_dest/audio_42@16-12-2020_15-19-02.mp3",
+      "id": 118,
+      "next_text": "Incertezas"
+  },
+
+  {
+      "from": "Alberto Harres",
+      "file": "convert_dest/audio_43@16-12-2020_15-23-28.mp3",
+      "id": 120,
+      "next_text": "Minutos de silêncio"
+  },
+
+  {
+      "from": "Josh",
+      "file": "convert_dest/audio_44@16-12-2020_18-02-27.mp3",
+      "id": 124,
+      "next_text": "dog's dinner"
+  },
+
+  {
+      "from": "Josh",
+      "file": "convert_dest/audio_45@16-12-2020_18-03-10.mp3",
+      "id": 126,
+      "next_text": "extended support bubble"
+  },
+
+  {
+      "from": "Antônio Hof",
+      "file": "convert_dest/audio_46@16-12-2020_19-05-24.mp3",
+      "id": 131,
+      "next_text": "Máscara"
+  },
+
+  {
+      "from": "Antônio Hof",
+      "file": "convert_dest/audio_47@16-12-2020_19-07-09.mp3",
+      "id": 133,
+      "next_text": "zigue-zague"
+  },
+
+  {
+      "from": "nilya musaeva",
+      "file": "convert_dest/audio_48@16-12-2020_19-08-12.mp3",
+      "id": 135,
+      "next_text": "надоело"
+  },
+
+  {
+      "from": "Bojana",
+      "file": "convert_dest/audio_49@16-12-2020_19-10-14.mp3",
+      "id": 137,
+      "next_text": "nedostajanje"
+  },
+
+  {
+      "from": "Bojana",
+      "file": "convert_dest/audio_50@16-12-2020_19-10-23.mp3",
+      "id": 139,
+      "next_text": "vreme"
+  },
+
+  {
+      "from": "Bojana",
+      "file": "convert_dest/audio_51@16-12-2020_19-10-59.mp3",
+      "id": 141,
+      "next_text": "bliskost"
+  },
+
+  {
+      "from": "Bojana",
+      "file": "convert_dest/audio_52@16-12-2020_19-11-42.mp3",
+      "id": 143,
+      "next_text": "daljina"
+  },
+
+  {
+      "from": "Bojana",
+      "file": "convert_dest/audio_53@16-12-2020_19-22-23.mp3",
+      "id": 145,
+      "next_text": "ograničenost"
+  },
+
+  {
+      "from": "Soumya Hfk",
+      "file": "convert_dest/audio_54@16-12-2020_21-41-43.mp3",
+      "id": 148,
+      "next_text": "সবাই ভালো থেকো"
+  },
+
+
+
+
+  {
+      "from": "Alberto Harres",
+      "file": "convert_dest/audio_55@17-12-2020_10-32-05.mp3",
+      "id": 153,
+      "next_text": "Luto"
+  },
+
+  {
+      "from": "Victor Artiga",
+      "file": "convert_dest/audio_56@17-12-2020_11-50-21.mp3",
+      "id": 156,
+      "next_text": "Distancia"
+  },
+
+  {
+      "from": "Victor Artiga",
+      "file": "convert_dest/audio_57@17-12-2020_11-50-29.mp3",
+      "id": 158,
+      "next_text": "Introspección"
+  },
+
+  {
+      "from": "Victor Artiga",
+      "file": "convert_dest/audio_58@17-12-2020_11-50-39.mp3",
+      "id": 160,
+      "next_text": "Amistades"
+  },
+
+  {
+      "from": "Victor Artiga",
+      "file": "convert_dest/audio_59@17-12-2020_11-50-48.mp3",
+      "id": 162,
+      "next_text": "Cercania"
+  },
+
+  {
+      "from": "Victor Artiga",
+      "file": "convert_dest/audio_60@17-12-2020_11-50-57.mp3",
+      "id": 164,
+      "next_text": "Nostalgia"
+  },
+
+  {
+      "from": "Josh",
+      "file": "convert_dest/audio_61@17-12-2020_12-06-09.mp3",
+      "id": 166,
+      "next_text": "church bells and sirens"
+  },
+
+  {
+      "from": "Josh",
+      "file": "convert_dest/audio_62@17-12-2020_12-06-22.mp3",
+      "id": 168,
+      "next_text": "pandemonium"
+  },
+
+
+  {
+      "from": "nilya musaeva",
+      "file": "convert_dest/audio_63@17-12-2020_12-16-03.mp3",
+      "id": 173,
+      "next_text": "মন মরা ভাব"
+  },
+
+  {
+      "from": "Debaditya Bhowmik",
+      "file": "convert_dest/audio_64@17-12-2020_12-37-12.mp3",
+      "id": 181,
+      "next_text": ""
+  },
+  {
+      "from": "Julia",
+      "file": "convert_dest/audio_65@17-12-2020_12-37-54.mp3",
+      "id": 182,
+      "next_text": "hamstern"
+  },
+
+
+  {
+      "from": "Debaditya Bhowmik",
+      "file": "convert_dest/audio_66@17-12-2020_12-38-13.mp3",
+      "id": 185,
+      "next_text": ""
+  },
+  {
+      "from": "Julia",
+      "file": "convert_dest/audio_67@17-12-2020_12-41-41.mp3",
+      "id": 186,
+      "next_text": "vereinsamen"
+  },
+
+  {
+      "from": "Ayse Seyhan",
+      "file": "convert_dest/audio_68@17-12-2020_13-42-41.mp3",
+      "id": 188,
+      "next_text": "Kırlangıç"
+  },
+
+  {
+      "from": "Ayse Seyhan",
+      "file": "convert_dest/audio_69@17-12-2020_13-43-02.mp3",
+      "id": 190,
+      "next_text": "Huzur"
+  },
+
+  {
+      "from": "Ayse Seyhan",
+      "file": "convert_dest/audio_70@17-12-2020_13-43-14.mp3",
+      "id": 192,
+      "next_text": "Tesadüf"
+  },
+
+  {
+      "from": "Ayse Seyhan",
+      "file": "convert_dest/audio_71@17-12-2020_13-43-29.mp3",
+      "id": 194,
+      "next_text": "Afilli"
+  },
+
+  {
+      "from": "Ayse Seyhan",
+      "file": "convert_dest/audio_72@17-12-2020_13-43-48.mp3",
+      "id": 196,
+      "next_text": "Serzeniş"
+  },
+
+  {
+      "from": "Ayse Seyhan",
+      "file": "convert_dest/audio_73@17-12-2020_13-46-23.mp3",
+      "id": 198,
+      "next_text": "Sadakat"
+  },
+
+  {
+      "from": "Ayse Seyhan",
+      "file": "convert_dest/audio_74@17-12-2020_13-46-50.mp3",
+      "id": 200,
+      "next_text": "Masum"
+  },
+
+  {
+      "from": "Ayse Seyhan",
+      "file": "convert_dest/audio_75@17-12-2020_13-46-59.mp3",
+      "id": 202,
+      "next_text": "Masumiyet"
+  },
+
+  {
+      "from": "Ayse Seyhan",
+      "file": "convert_dest/audio_76@17-12-2020_13-47-15.mp3",
+      "id": 204,
+      "next_text": "Aşk"
+  },
+
+  {
+      "from": "Ayse Seyhan",
+      "file": "convert_dest/audio_77@17-12-2020_13-47-33.mp3",
+      "id": 206,
+      "next_text": "Ansızın"
+  },
+
+  {
+      "from": "Chi Him Chik",
+      "file": "convert_dest/audio_78@17-12-2020_13-57-42.mp3",
+      "id": 208,
+      "next_text": "封關呀仆街"
+  },
+
+  {
+      "from": "Chi Him Chik",
+      "file": "convert_dest/audio_79@17-12-2020_13-58-13.mp3",
+      "id": 211,
+      "next_text": "帶口罩呀屌你老母"
+  },
+
+  {
+      "from": "Alberto Harres",
+      "file": "convert_dest/audio_80@17-12-2020_14-15-18.mp3",
+      "id": 213,
+      "next_text": "Falta de ar"
+  },
+
+  {
+      "from": "Antônio Hof",
+      "file": "convert_dest/audio_81@17-12-2020_14-20-30.mp3",
+      "id": 215,
+      "next_text": "solidão"
+  },
+
+  {
+      "from": "Antônio Hof",
+      "file": "convert_dest/audio_82@17-12-2020_14-20-58.mp3",
+      "id": 217,
+      "next_text": "ermo"
+  },
+
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_83@17-12-2020_17-33-22.mp3",
+      "id": 220,
+      "next_text": "第一波"
+  },
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_84@17-12-2020_17-33-30.mp3",
+      "id": 221,
+      "next_text": "第二波"
+  },
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_85@17-12-2020_17-33-38.mp3",
+      "id": 222,
+      "next_text": "第三波"
+  },
+  {
+      "from": "Kazuki",
+      "file": "convert_dest/audio_86@17-12-2020_17-33-56.mp3",
+      "id": 223,
+      "next_text": "第四波"
+  },
+  {
+      "from": "Alberto Harres",
+      "file": "convert_dest/audio_87@18-12-2020_17-12-23.mp3",
+      "id": 224,
+      "next_text": "O Mundo"
+  },
+
+  {
+      "from": "Hanna",
+      "file": "convert_dest/audio_88@18-12-2020_18-05-21.mp3",
+      "id": 226,
+      "next_text": "Салідарнасць"
+  },
+
+  {
+      "from": "Hanna",
+      "file": "convert_dest/audio_89@18-12-2020_18-06-14.mp3",
+      "id": 228,
+      "next_text": "Рэвалюцыя"
+  },
+
+  {
+      "from": "Hanna",
+      "file": "convert_dest/audio_90@18-12-2020_18-06-45.mp3",
+      "id": 230,
+      "next_text": "Супольнасць"
+  }
+]
